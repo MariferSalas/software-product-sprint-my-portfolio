e5b5eed4ce7d51d47290335c1ca94f675e3e1e42
MariferSalas <a01176943@itesm.mx>
2021-03-08 23:46:49 +0000

    Added step 3 of week 2, passing information with JSON

--- portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/PortfolioServlet.java
@@ -1,5 +1,20 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.sps.servlets;
 
+import com.google.gson.Gson;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +25,26 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/randomLyrics")
 public class PortfolioServlet extends HttpServlet {
 
+    private final String[] lyricslist = {"Why cant you see? you belong with me", "Back then I swore I was gonna marry him someday but I realized some bigger dreams of mine", "You call me up again just to break me like a promise"}; 
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("You belong with me");
+    
+    // Convert the string array to JSON
+    String json = convertToJsonUsingGson(lyricslist);
+    
+    // Send the JSON as the response
+    response.setContentType("application/json;");
+    response.getWriter().println(json);
+  }
+
+  /**
+   * Converts a ServerStats instance into a JSON string using the Gson library. Note: We first added
+   * the Gson library dependency to pom.xml.
+   */
+  private String convertToJsonUsingGson(String[] lyrics) {
+    Gson gson = new Gson();
+    String json = gson.toJson(lyrics);
+    return json;
   }
 }
