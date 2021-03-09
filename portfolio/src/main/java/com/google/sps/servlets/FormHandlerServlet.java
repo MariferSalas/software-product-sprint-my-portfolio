e02808846fb145b13cc1371860d9963586b9cc8f
MariferSalas <a01176943@itesm.mx>
2021-03-09 01:44:44 +0000

    Adapted form and created a redirected site

--- portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/FormHandlerServlet.java
@@ -19,6 +19,8 @@ public class FormHandlerServlet extends HttpServlet {
     System.out.println("You submitted: " + textValue);
 
     // Write the value to the response so the user can see it.
-    response.getWriter().println("You submitted: " + textValue);
+    //response.getWriter().println("You submitted: " + textValue);
+
+    response.sendRedirect("https://msalas-sps-spring21.uc.r.appspot.com/");
   }
 }
